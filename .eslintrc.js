089265866f10de4720c1a6138b56e3c3d8ecc457
ederpbj <ederpbj@yahoo.com.br>
2020-01-22 16:22:56 -0300

    my-money 1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
-module.exports = {  
-    root: true,  
-    plugins: ['node'],  
-    extends: ['plugin:node/recommended', 'prettier'],  
-    env: {  
-      node: true  
-    }  
-  };
+module.exports = {
+  root: true,
+  plugins: ['node'],
+  extends: ['plugin:node/recommended', 'prettier'],
+  env: {
+    node: true
+  }
+};
+
+  //Com isso estamos usando as regras recomendadas para desenvolvimento Node.js.
